537c516133011f5b2e7ba7bbfb115f96bbbc96a9
MinecraftDawn <30753602+MinecraftDawn@users.noreply.github.com>
2018-05-24 16:34:31 +0800

    Add files via upload

---
+++ 骰子.cpp
@@ -0,0 +1,443 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+#include <cstring>
+
+using namespace std;
+
+class PokerCard{
+public:
+    PokerCard(){}
+
+    PokerCard(int num){
+        numToPoker(num);
+        _sended = false;
+    }
+
+    int getColor(){
+        return _color;
+    }
+
+    string getColorSt(){
+        switch(_color){
+            case 0:
+                return "梅花";
+                break;
+
+            case 1:
+                return "方塊";
+                break;
+
+            case 2:
+                return "紅心";
+                break;
+
+            case 3:
+                return "黑桃";
+                break;
+
+            default:
+                return "Error";
+                break;
+        }
+    }
+
+    int getNum(){
+        return _number;
+    }
+
+    bool isSended(){
+        return _sended;
+    }
+
+    void send(){
+        _sended = true;
+    }
+
+    void Recover(){
+        _sended = false;
+    }
+
+private:
+    void numToPoker(int num){
+        _color = (num-1)/13;
+        _number = (num-1)%13+1;
+
+        if(_color > 4 || _number > 13 ||
+           (_color == 0 && _number == 0)){
+            cout << "[警告] 花色或數字有問題！" << endl;
+        }
+    }
+
+private://屬性
+    string _colorSt;
+    int _color;
+    int _number;
+    bool _sended;
+};
+
+class PokerCards{
+public:
+    PokerCards(){
+
+        Shuffle();
+
+        for(int i =1;i<=52;i++){ //初始化卡片
+            card[i] = PokerCard(i);
+        }
+    }
+
+    void Shuffle(){
+        for(int i =1;i<=52;i++){ //回收卡片
+            card[i].Recover();
+        }
+    }
+
+    PokerCard* getCard(int num){
+        return &(card[num]);
+    }
+
+    bool checkHaveCard(){
+        for(int i =1;i<=52;i++){
+            if(! card[i].isSended())
+                return true;
+        }
+        return false;
+    }
+
+private:
+
+private://屬性
+    PokerCard card[53];
+};
+
+class Dice{
+public:
+    int getPoint(){
+        return _point;
+    }
+    void setPoint(int point){
+        if(point < 1 || point > 6){
+            cout << "[警告]骰子點數有誤" << endl;
+        }
+        _point = point;
+    }
+    void randPoint(){
+        _point = rand()%6 +1;
+    }
+private:
+    int _point;
+};
+
+class IMenu{
+public:
+    IMenu(){
+        _cpuWin = 0;
+        _playerWin = 0;
+    }
+
+    void showRound(){
+        cout << "第 " <<  getRound() << " 回合\t"
+             << "玩家勝場 " << getPlayerWin()  << "\t"
+             << "電腦勝場"  << getComputerWin() << endl;
+    }
+
+    void run();
+
+protected:
+    void showMenu();
+
+    void srartGame();
+
+    int getRound(){
+        return _cpuWin + _playerWin;
+    }
+
+    int getComputerWin(){
+        return _cpuWin;
+    }
+
+    int getPlayerWin(){
+        return _playerWin;
+    }
+
+protected:
+    int _select;
+    int _cpuWin;
+    int _playerWin;
+    bool size;//True->大 , False->小
+};
+
+class MenuPoker : virtual public IMenu{
+public:
+
+    void run(){
+        do{
+            showMenu();
+
+            cin >> _select;
+
+            if(_select < 1 || _select > 5){
+                cout << "[警告] 輸入的數值有誤，請重新輸入" << endl;
+                continue;
+            }
+
+            switch(_select){
+                case 1://洗牌
+                    cards.Shuffle();
+                    break;
+
+                case 2://比大
+                    size = true;
+                    srartGame();
+                    break;
+
+                case 3://比小
+                    size = false;
+                    srartGame();
+                    break;
+
+                case 4://場數
+                    showRound();
+                    break;
+
+                case 5://離開
+                    break;
+            }
+
+        }while(_select != 5);
+    }
+
+private:
+    void showMenu(){
+        cout << "(1).洗牌"	<< endl
+             << "(2).比大"	<< endl
+             << "(3).比小"	<< endl
+             << "(4).勝場"	<< endl
+             << "(5).離開"	<< endl
+             << "請輸入選擇項目？" 	<< endl;
+    }
+
+    void sendCard(){
+        srand(time(NULL));
+
+        int card;
+        PokerCard *ptr;
+
+        /***************************
+        發玩家手排
+        ****************************/
+        do{
+            card = rand()%52+1;
+
+            ptr = cards.getCard(card);
+
+        }while( ptr->isSended());
+        ptr->send();
+        playerCard = *ptr;
+
+        /***************************
+        發電腦手排
+        ****************************/
+        do{
+            card = rand()%52+1;
+
+            ptr = cards.getCard(card);
+
+        }while(ptr->isSended());
+        ptr->send();
+        cpuCard = *ptr;
+
+    }
+
+    void srartGame(){
+        if(!cards.checkHaveCard()){
+            cout << "沒有排摟，請洗牌" << endl;
+            return;
+        }
+
+        sendCard();
+
+        cout << "玩家手排： " << playerCard.getColorSt() << playerCard.getNum() << endl
+             << "電腦手排： " << cpuCard.getColorSt() << cpuCard.getNum() << endl;
+
+        bool playerBig;
+
+        playerBig = judgeSize(playerCard,cpuCard);
+
+        if(size){
+            if(playerBig){
+                cout << "玩家獲勝" << endl;
+                _playerWin++;
+            }else{
+                cout << "電腦獲勝" << endl;
+                _cpuWin++;
+            }
+        }else{
+            if(!playerBig){
+                cout << "玩家獲勝" << endl;
+                _playerWin++;
+            }else{
+                cout << "電腦獲勝" << endl;
+                _cpuWin++;
+            }
+        }
+
+        cout << endl;
+
+    }
+
+    bool judgeSize(PokerCard card1,PokerCard card2){//card1>card2 會回傳True;
+        if(card1.getNum() == card2.getNum()){
+
+            if(card1.getColor() > card2.getColor()){
+                return true;
+            }else{
+                return false;
+            }
+
+        }else if(card1.getNum() > card2.getNum()){
+            return true;
+        }else if(card1.getNum() < card2.getNum()){
+            return false;
+        }
+
+
+    }
+
+private:
+    PokerCards cards;
+    PokerCard playerCard;
+    PokerCard cpuCard;
+};
+
+class MenuDice : virtual public IMenu{
+public:
+    MenuDice(){
+        srand(time(NULL));
+        _round = 0;
+    }
+
+    void showMenu(){
+        cout << "(1).知識"	<< endl
+             << "(2).比大"	<< endl
+             << "(3).比小"	<< endl
+             << "(4).勝場"	<< endl
+             << "(5).離開"	<< endl
+             << "請輸入選擇項目？" 	<< endl;
+    }
+
+    void run(){
+        do{
+            showMenu();
+
+            cin >> _select;
+
+            if(_select < 1 || _select > 5){
+                cout << "[警告] 輸入的數值有誤，請重新輸入" << endl;
+                continue;
+            }
+
+            switch(_select){
+                case 1://小知識
+                    showKnowledge();
+                    break;
+
+                case 2://比大
+                    size = true;
+                    srartGame();
+                    break;
+
+                case 3://比小
+                    size = false;
+                    srartGame();
+                    break;
+
+                case 4://場數
+                    showRound();
+                    break;
+
+                case 5://離開
+                    break;
+            }
+        }while (_select != 5);
+    }
+
+
+private:
+    void showKnowledge(){
+        cout << "骰子對面加起來點數合為7唷" << endl;
+    }
+
+    void srartGame(){
+        throwDice();
+
+        _round++;
+
+        int Win = judgeSize(_plDice,_comDice);
+
+        if(Win == 2){
+            cout << "平手唷" << endl;
+            return;
+        }
+
+        cout << "玩家點數： " << _plDice.getPoint() << "點"<< endl
+             << "電腦點數： " << _comDice.getPoint() << "點"<< endl;
+
+        if(size){//比大
+            if(Win == 1){
+                cout << "玩家獲勝" << endl;
+                _playerWin++;
+            }else if(Win == 3){
+                cout << "電腦獲勝" << endl;
+                _cpuWin++;
+            }
+        }else{//比小
+            if(Win == 3){
+                cout << "玩家獲勝" << endl;
+                _playerWin++;
+            }else if(Win == 1){
+                cout << "電腦獲勝" << endl;
+                _cpuWin++;
+            }
+        }
+    }
+
+    void throwDice(){
+        _plDice.randPoint();
+        _comDice.randPoint();
+    }
+
+    int judgeSize(Dice player,Dice computer){
+        if(player.getPoint() > computer.getPoint()){
+            return 1; //Player big
+        }else if(player.getPoint() == computer.getPoint()){
+            return 2; //Same size
+        }else{
+            return 3; //Computer big
+        }
+
+
+    }
+
+    int getRound(){
+        return _round;
+    }
+
+    void showRound(){
+        cout << "第 " <<  getRound() << " 回合\t"
+             << "玩家勝場 " << getPlayerWin()  << "\t"
+             << "電腦勝場"  << getComputerWin() << endl;
+    }
+private:
+    Dice _plDice;
+    Dice _comDice;
+    int _round;
+};
+
+int main(){
+    MenuPoker p;
+    MenuDice d;
+    d.run();
+
+}
